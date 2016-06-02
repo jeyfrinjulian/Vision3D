cb926da8af1c7e212aa98edcb1a1ed7af831c797
RemoRemovich <borismednikov@gmail.com>
2016-06-01 23:17:23 -0400

    Update main.py

--- main.py
+++ main.py
@@ -6,5 +6,5 @@ class MyApp(App):
     def build(self):
         return Label(text='Hello world')
 
-    if __name__ == '__main__':
-        MyApp().run()
+if __name__ == '__main__':
+    MyApp().run()
